c6f45b2e5807378cd94d4a2ecc00ae052af1be71
Carolyn McNeillie <cmcneillie@theworkinggroup.ca>
2020-03-26 15:09:11 -0400

    Exercise 2

--- lessons/02-state/exercise/LoginForm.js
+++ lessons/02-state/exercise/LoginForm.js
@@ -8,6 +8,11 @@ import { login } from "app/utils"
 // export default LoginFormFinal
 
 export default function LoginForm() {
+  const [showPassword, setShowPassword] = useState(false)
+  const handleToggle = e => {
+    setShowPassword(e.currentTarget.checked)
+  }
+
   return (
     <form>
       <VisuallyHidden>
@@ -25,7 +30,7 @@ export default function LoginForm() {
       </VisuallyHidden>
       <input
         id="login:password"
-        type="password"
+        type={showPassword ? "text" : "password"}
         className="inputField"
         placeholder="Password"
       />
@@ -36,6 +41,7 @@ export default function LoginForm() {
             className="passwordCheckbox"
             type="checkbox"
             defaultChecked={false}
+            onChange={handleToggle}
           />{" "}
           show password
         </label>
